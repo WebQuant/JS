c2996ae09cdc94a482f6587d71c6526141f361a3
Quant <alekseev.aleksai@yandex.ru>
2020-11-13 12:52:28 +0300

    root of the quadratic equation

--- lab_1/js/script.js
+++ lab_1/js/script.js
@@ -19,3 +19,25 @@ function result() {
     document.getElementById('result').innerHTML = s;
 
 }
+
+function result_x() {
+
+    var a, b, c, x1, x2, d;
+
+    a = document.getElementById('inpx_a').value;
+    a = parseInt(a);
+    b = document.getElementById('inpx_b').value;
+    b = parseInt(b);
+    c = document.getElementById('inpx_c').value;
+    c = parseInt(c);
+
+    d = Math.pow(b, 2) - 4 * a * c;
+    x1 = (-(b) + Math.sqrt(d)) / (2 * a);
+    x2 = (-(b) - Math.sqrt(d)) / (2 * a);
+
+    alert("Ready!");
+
+    document.getElementById('result_x1').innerHTML = x1;
+    document.getElementById('result_x2').innerHTML = x2;
+
+}
